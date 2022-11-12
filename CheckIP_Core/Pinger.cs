8e37cef8763e45265abde9a55eb6237e678f7299
Daniil Saushkin <0suffix0@gmail.com>
2022-11-12 18:41:09 +0700

    Create CheckIP method

--- CheckIP_Core/Pinger.cs
+++ CheckIP_Core/Pinger.cs
@@ -1,7 +1,21 @@
-﻿namespace CheckIP_Core
-{
-    public class Pinger
-    {
+﻿using System.Net.NetworkInformation;
 
+namespace CheckIP_Core
+{
+    public sealed class Pinger
+    {
+        private readonly Ping _pingSender = new();
+
+        public PingReply CheckIP(string IP, int timeout = 250)
+        {
+            try
+            {
+                return _pingSender.Send(IP.Trim(), timeout);
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
