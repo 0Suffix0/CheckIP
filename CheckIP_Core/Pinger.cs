c9cceb7c155219595e69d1648f45d2b442a59a09
Daniil Saushkin <0suffix0@gmail.com>
2022-11-13 20:15:04 +0700

    Added comments and added Exception to catch

--- CheckIP_Core/Pinger.cs
+++ CheckIP_Core/Pinger.cs
@@ -6,18 +6,30 @@ namespace CheckIP_Core
     {
         private readonly Ping _pingSender = new();
 
+        /// <summary>
+        /// Ping just one IP-adress.
+        /// </summary>
+        /// <param name="IP"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
         public PingReply CheckIP(string IP, int timeout = 250)
         {
             try
             {
                 return _pingSender.Send(IP.Trim(), timeout);
             }
-            catch
+            catch (Exception e)
             {
-                return null;
+                throw new Exception("Unknown error\n" + e.ToString());
             }
         }
 
+        /// <summary>
+        /// Pings IP-adresses in an IEnumerable collection. 
+        /// </summary>
+        /// <param name="IPlist"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
         public List<PingReply> CheckIP(IEnumerable<string> IPlist, int timeout = 250)
         {
             List<PingReply> pingReplies = new ();
@@ -28,9 +40,9 @@ namespace CheckIP_Core
                     pingReplies.Add(_pingSender.Send(IP.Trim(), timeout));
                 return pingReplies;
             }
-            catch
+            catch (Exception e)
             {
-                return null;
+                throw new Exception("Unknown error\n" + e.ToString());
             }
         }
     }
