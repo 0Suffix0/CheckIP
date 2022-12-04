d833d88994b0795b327b8ce0ad211261de467d05
Daniil Saushkin <0suffix0@gmail.com>
2022-12-04 16:05:45 +0700

    CLI

--- CheckIP_CLI/Program.cs
+++ CheckIP_CLI/Program.cs
@@ -1,8 +1,19 @@
-﻿namespace CheckIP_CLI;
+﻿using CheckIP_Core;
+using Spectre.Console;
+
+namespace CheckIP_CLI;
 
 internal sealed class Program
 {
+    static Pinger pinger = new();
     private static void Main(string[] args)
     {
+        var table = new Table();
+
+        table.AddColumns("IP", "Status");
+        table.Columns[1].Width(50);
+
+        pinger.CheckIPAsync("yandex.ru", new CancellationTokenSource().Token);
+
     }
 }
