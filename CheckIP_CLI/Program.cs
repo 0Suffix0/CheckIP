0265bbcbb27dd3728d24ad1958220badeefdbab1
Хатнянский Максим <maksimhatnynsky@gmail.com>
2022-12-04 12:00:06 +0300

    SendAsync
    Реализован асинхронный метод проверки доступности IP

--- CheckIP_CLI/Program.cs
+++ CheckIP_CLI/Program.cs
@@ -1,26 +1,8 @@
-﻿using CheckIP_Core;
-using Spectre.Console;
-using System.Net.NetworkInformation;
+﻿namespace CheckIP_CLI;
 
-namespace CheckIP_CLI
+internal sealed class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static readonly Ping _pingSender = new();
-
-        static void Main(string[] args)
-        {
-            _pingSender.PingCompleted += (s, e) => {
-                if(e.Reply is not null)
-                    Console.WriteLine(e.Reply.Status);
-            };
-
-            while (true)
-            {
-                Task.Delay(1000);
-                _pingSender.SendAsync("yandex.ru", 250);
-            }
-            Console.ReadKey();
-        }
     }
 }
--- CheckIP_Core/Pinger.cs
+++ CheckIP_Core/Pinger.cs
@@ -6,6 +6,13 @@ namespace CheckIP_Core
     {
         private readonly Ping _pingSender = new();
 
+        public event EventHandler<object>? StatusIP;
+
+        public Pinger()
+        {
+            _pingSender.PingCompleted += (s, e) => StatusIP?.Invoke(this, e);
+        }
+
         /// <summary>
         /// Ping just one IP-adress.
         /// </summary>
@@ -24,15 +31,23 @@ namespace CheckIP_Core
             }
         }
 
+        public void CheckIPAsync(string IP, CancellationToken token, int timeout = 250)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                _pingSender.SendAsync(IP.Trim(), timeout, token);
+            }
+        }
+
         /// <summary>
-        /// Pings IP-adresses in an IEnumerable collection. 
+        /// Pings IP-adresses in an IEnumerable collection.
         /// </summary>
         /// <param name="IPlist"></param>
         /// <param name="timeout"></param>
         /// <returns></returns>
         public IEnumerable<PingReply> CheckIP(IEnumerable<string> IPlist, int timeout = 250)
         {
-            List<PingReply> pingReplies = new ();
+            List<PingReply> pingReplies = new();
 
             try
             {
